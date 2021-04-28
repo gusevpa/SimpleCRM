2314dc3e786d3c736f1691f3bba6d85477224a77
Admin <flenivetc@gmail.com>
2021-04-28 20:21:57 +0300

    small fixes

--- src/main/java/com/example/users/Configuration/TableNameConfig.java
+++ src/main/java/com/example/users/Configuration/TableNameConfig.java
@@ -5,9 +5,15 @@ import org.hibernate.engine.jdbc.env.spi.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.context.annotation.*;
 
+/**
+ * Переназначение спрингового генератора имен на значение из application.properties
+ * В данном варианте переназназначает имя таблицы по-умолчанию для UserEntity
+ */
 @Configuration
 public class TableNameConfig {
-
+    /**
+     * Берем значение userTableName из application.properties
+     */
     @Value("${user.table.name}")
     private String userTableName;
 
@@ -17,7 +23,12 @@ public class TableNameConfig {
     }
 
     class PhysicalNamingImpl extends PhysicalNamingStrategyStandardImpl {
-
+        /**
+         *
+         * @param name - перехватываем имя entity - класса и
+         *             в случае совпадения с искомым - заменяем
+         *             на userTableName, взятый из пропертей
+         */
         @Override
         public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment context) {
             switch (name.getText()) {
